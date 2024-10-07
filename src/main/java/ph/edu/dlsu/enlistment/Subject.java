594e2c95a6b307e24dce13a9da5212171250e032
Adighost135 <92373537+Adighost135@users.noreply.github.com>
2024-10-07 21:19:54 +0800

    Subject Initial Commit
    Includes checkPrerequisites

---
+++ src/main/java/ph/edu/dlsu/enlistment/Subject.java
@@ -0,0 +1,57 @@
+package ph.edu.dlsu.enlistment;
+
+import java.util.*;
+
+class Subject {
+    private final String subjectId;
+    private final float units;
+    private final boolean isLaboratory;
+    private final Collection<Subject> prerequisites;
+
+    Subject(String subjectId, float units, boolean isLaboratory, Collection<Subject> prerequisites){
+        Objects.requireNonNull(subjectId);
+        Objects.requireNonNull(units);
+        Objects.requireNonNull(isLaboratory);
+        Validate.isTrue.isAlphanumeric(subjectId, "subjectId must be alphanumeric, was: " +subjectId);
+        if (units < 0 || units > 4) {
+            throw new IllegalStateException("Units can't be negative or above 4. was: " + units);
+        }
+        this.subjectId = subjectId;
+        this.units = units;
+        this.isLaboratory = isLaboratory;
+        this.prerequisites = new Collection<> prerequisites;
+    }
+
+
+    /* If any prerequisite is not found in the 
+    takenSubjects collection, the method immediately 
+    returns false, indicating that not all prerequisites 
+    have been met.
+    */
+    boolean checkPrerequisites(Collection<Subject> takenSubjects) {
+        for (Subject prerequisite : prerequisites) {
+            if (!takenSubjects.contains(prerequisite)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return subjectId;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Subject subject = (Subject) o;
+        return subjectId.equals(subject.subjectId);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(subjectId);
+    }
+}
