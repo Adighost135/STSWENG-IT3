0932da39e5b6bf0397cd38d78776d542f5cd64bd
ReythemGespenst <roncajumban@gmail.com>
2024-10-09 15:25:48 +0800

    Updated Schedule.java
    imported NumberUtils lib, Validate lib.
    
    Added new data: periodStart and periodEnd.
    
    Validation for non Null and isDigits for periodStart and Period End.
    
    Added overriding for equals to check if there is a conflict between schedules

--- src/main/java/ph/edu/dlsu/enlistment/Schedule.java
+++ src/main/java/ph/edu/dlsu/enlistment/Schedule.java
@@ -1,18 +1,31 @@
 package ph.edu.dlsu.enlistment;
 
 import java.util.*;
+import org.apache.commons.lang3.math.NumberUtils;
+import org.apache.commons.lang3.Validate;
 
-record Schedule(Days days, Period period) {
+record Schedule(Days days, String periodStart, String periodEnd) {
 
     Schedule{
         Objects.requireNonNull(days);
-        Objects.requireNonNull(period);
+        Objects.requireNonNull(periodStart);
+        Objects.requireNonNull(periodEnd);
+        Validate.isTrue(NumberUtils.isDigits(periodStart), "Period must be numeric, was: " + periodStart);
+        Validate.isTrue(NumberUtils.isDigits(periodEnd), "Period must be numeric, was: " + periodEnd);
     }
 
     @Override
     public String toString(){
         // TF H0830, WS H1000
-        return days + " " + period;
+        return days + " " + periodStart + "-" + periodEnd;
+    }
+
+    @Override
+    public final boolean equals(Object o){
+        if(){
+            return true;
+        }
+        return false;
     }
 }
 
@@ -20,9 +33,7 @@ enum Days {
     MTH, TF, WS
 }
 
-enum Period{
-    H0830, H1000, H1130, H1300, H1430, H1600
-}
-
-
+//enum Period{
+//    H0830, H1000, H1130, H1300, H1430, H1600
+//}
 
