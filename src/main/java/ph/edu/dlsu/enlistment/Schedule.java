9da325a58eb40d9c45178782eff2870e41b486fa
ReythemGespenst <roncajumban@gmail.com>
2024-10-09 20:39:07 +0800

    Updated Schedule.java
    Validators are added to check for requirements
    
    overriden equals into a boolean to check, it is used to check if there is an overlap between two schedules based on the start time and end time between two schedules

--- src/main/java/ph/edu/dlsu/enlistment/Schedule.java
+++ src/main/java/ph/edu/dlsu/enlistment/Schedule.java
@@ -10,8 +10,22 @@ record Schedule(Days days, String periodStart, String periodEnd) {
         Objects.requireNonNull(days);
         Objects.requireNonNull(periodStart);
         Objects.requireNonNull(periodEnd);
-        Validate.isTrue(NumberUtils.isDigits(periodStart), "Period must be numeric, was: " + periodStart);
-        Validate.isTrue(NumberUtils.isDigits(periodEnd), "Period must be numeric, was: " + periodEnd);
+        Validate.isTrue(NumberUtils.isDigits(periodStart), "Period Start must be numeric, was: " + periodStart);
+        Validate.isTrue(NumberUtils.isDigits(periodEnd), "Period End must be numeric, was: " + periodEnd);
+        if(Integer.parseInt(periodStart) % 100 != 0 && Integer.parseInt(periodStart) % 100 != 30) throw new IllegalArgumentException(
+                "Period does not start at the top or bottom of each hour, was" + periodStart);
+
+        if(Integer.parseInt(periodEnd) % 100 != 0 && Integer.parseInt(periodEnd) % 100 != 30) throw new IllegalArgumentException(
+                "Period does not end at the top or bottom of each hour, was" + periodEnd );
+
+        if(Integer.parseInt(periodStart) >= Integer.parseInt(periodEnd)) throw new IllegalArgumentException(
+            "Start of the period should not be the same or exceed the end of the period");
+
+        if(Integer.parseInt(periodStart) < 830 || Integer.parseInt(periodStart) > 1730) throw new IllegalArgumentException(
+            "Start of the period should not be outside the bounds of 830 to 1730");
+
+        if(Integer.parseInt(periodEnd) < 830 || Integer.parseInt(periodEnd) > 1730) throw new IllegalArgumentException(
+            "End of the period should not be outside the bounds of 830 to 1730");
     }
 
     @Override
@@ -22,13 +36,24 @@ record Schedule(Days days, String periodStart, String periodEnd) {
 
     @Override
     public final boolean equals(Object o){
-        if(){
-            return true;
+        if(this == o)return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Schedule schedule = (Schedule) o;
+        int thisStart = Integer.parseInt(this.periodStart);
+        int thisEnd = Integer.parseInt(this.periodEnd);
+        int otherStart = Integer.parseInt(schedule.periodStart);
+        int otherEnd = Integer.parseInt(schedule.periodEnd);
+        if (this.days == schedule.days){
+            if(thisStart < otherEnd && thisEnd > otherStart){
+                return true;
+            }
+            if(!(thisStart > otherEnd || otherStart > thisEnd)){
+                return true;
+            }
         }
         return false;
     }
 }
-
 enum Days {
     MTH, TF, WS
 }
